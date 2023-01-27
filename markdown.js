b1d4948ec71432e13ab5f00aa24507765d7ae030
Sarah Mullock <smullock@hotmail.com>
2023-01-27 22:29:03 +1100

    add markdown

---
+++ markdown.js
@@ -0,0 +1,33 @@
+function markdown(data) {
+    return `
+# ${data.title}
+## Description
+${data.description}
+## Table of Contents 
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Contributing](#contributing)
+* [Test](#Test)
+* [Questions](#questions)
+
+## Installation
+${data.installation} 
+
+## Usage 
+ ${data.usage}
+## License 
+ ${data.license}
+## Contributing 
+ ${data.contributing}
+## Test 
+ ${data.tests}
+## Questions 
+Any questions , please contact me at the below details
+### My Github Profile:${data.github} (https://github.com/${data.github})
+### Email: ${data.email}
+`
+
+}
+
+module.exports = markdown;
